402c076be3b9ed9fa5679692f734bcbf230152bd
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-27 21:36:23 -0500

    :sparkles: Getters y Setters

---
+++ src/06-gettersSetters.js
@@ -0,0 +1,38 @@
+const hasCopyright = false
+
+class Manga {
+  #mangaka;
+  #name;
+
+  constructor({ name, mangaka }) {
+    this.#name = name;
+    this.#mangaka = mangaka;
+  }
+
+  get mangaka() {
+    return this.#mangaka;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(name) {
+    if (hasCopyright) {
+      console.log('This Manga name has copyright!');
+    } else {
+      this.#name = name;
+    }
+  }
+}
+
+const REQUIEM_ADVENTURES = new Manga({
+  name: 'Requiem Adventures',
+  mangaka: 'UltiRequiem',
+});
+
+console.log(REQUIEM_ADVENTURES);
+console.log(REQUIEM_ADVENTURES.mangaka);
+console.log(REQUIEM_ADVENTURES.name);
+REQUIEM_ADVENTURES.name = 'Adventures Requiem?';
+console.log(REQUIEM_ADVENTURES.name);
