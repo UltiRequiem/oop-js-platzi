4561bd9b6750a8540c9844ccb8483d8f04550383
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-28 10:42:03 -0500

    :sparkles: Modules
    I FEEL THE POWER IN MY VEINS

--- src/07-modules/gopher.mjs
+++ src/07-modules/gopher.mjs
@@ -1,13 +1,44 @@
-const giveHouse = (id) => `House ${id}`;
+const giveHouse = (id, name) => `The ID of ${name}'s house is ${id}.`;
+const giveHug = (name, times) => `${name} has give you a hug ${times} times!`;
 
 class Gopher {
+  #name;
+  #id;
   constructor({ name = 'Gopherin', id }) {
-    this.name = name;
-    this.id = id;
+    this.#name = name;
+    this.#id = id;
   }
 
   giveHouse() {
-    return giveHouse(this.id);
+    return giveHouse(this.#id, this.#name);
+  }
+
+  giveHug(times) {
+    return giveHug(this.#name, times);
+  }
+
+  get id() {
+    return `That's a secret!`;
+  }
+
+  get name() {
+    return `That's a secret!`;
+  }
+
+  set name(name) {
+    if (name === this.#name) {
+      console.log(`That is already the name of your Gopher!`);
+    } else {
+      console.log(`You cannot change the name of your Gopher!`);
+    }
+  }
+
+  set id(id) {
+    if (id === this.#id) {
+      console.log(`That is already the ID of your Gopher!`);
+    } else {
+      console.log(`You cannot change the ID of your Gopher!`);
+    }
   }
 }
 
--- src/07-modules/main.mjs
+++ src/07-modules/main.mjs
@@ -1,8 +1,18 @@
-// eslint-disable-next-line import/extensions
+// eslint-disable-next-line import/named
 import { Gopher } from './gopher.mjs';
 
-const GopherOne = new Gopher({ name: 'Jose', id: 4554 });
+const GopherOne = new Gopher({ name: 'Chunchumaru', id: 4554 });
 const GopherOneHouse = GopherOne.giveHouse();
+const GopherOneHug = GopherOne.giveHug(34);
 
-console.log(GopherOne);
+// GopherOne.name = 'aj' // You cannot change the name of your Gopher!
+// GopherOne.id = 'aj' // You cannot change the ID of your Gopher!
+
+// GopherOne.name = 'Chunchumaru' // That is already the name of your Gopher
+// GopherOne.id = 4554 // That is already the ID of your Gopher
+
+// console.log(GopherOne); Gopher {}
+// console.log(GopherOne.name) That's a secret!
+
+console.log(GopherOneHug);
 console.log(GopherOneHouse);
